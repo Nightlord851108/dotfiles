0f11cc83bad255a8331c9688012b9791f14c3f26
Patrick <nightlord851108@gmail.com>
2019-08-25 16:06:16 +0800

    Neofetch added

--- scripts/deploy_zsh.sh
+++ scripts/deploy_zsh.sh
@@ -11,6 +11,7 @@ brew_install '--HEAD universal-ctags'
 
 brew_install 'zsh'
 brew_install 'diff-so-fancy'
+brew_install 'neofetch'
 run_cmd 'git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"'
 print 'Remember to source ~/.zshrc'
 
