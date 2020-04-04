6fa7183bc83bda85c9a139c4e34de4ce62784fed
Patrick <hsiang851108@gmail.com>
2020-04-04 15:51:57 +0800

    Updated

--- scripts/deploy_fonts.sh
+++ scripts/deploy_fonts.sh
@@ -2,6 +2,6 @@
 source ./utils.sh
 
 print 'Homebrew Tap Fonts'
-run_cmd 'brew tap caskroom/fonts'
-cask_install 'font-hack-nerd-font'
+run_cmd 'brew tap homebrew/cask-fonts'
+# cask_install 'font-hack-nerd-font'
 cask_install 'font-sourcecodepro-nerd-font'
--- scripts/deploy_minimal_homebrew_cask_apps.sh
+++ scripts/deploy_minimal_homebrew_cask_apps.sh
@@ -2,7 +2,7 @@
 source ./utils.sh
 
 print 'Homebrew Tap Cask'
-run_cmd 'brew tap homebrew/cask-fonts'
+# run_cmd 'brew tap homebrew/cask-fonts'
 
 print 'Install Minimal Homebrew-Cask Apps'
 cask_install 'iterm2'
