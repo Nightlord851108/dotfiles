0a443052f4711243c7e894a6e1673af6d52c885c
Patrick <hsiang851108@gmail.com>
2020-04-11 09:00:06 +0800

    Maven & Gradle deploy added

--- scripts/link_all.sh
+++ scripts/link_all.sh
@@ -7,3 +7,6 @@ print 'Link Dotfiles'
 
 print 'Link Atom'
 link_dir $SOURCE'/atom/atom.symlink' $HOME'/.atom'
+
+print 'Link Maven'
+link_dir $SOURCE'/maven/apache-maven-3.6.3' '/usr/local/apache-maven-3.6.3'
