bd5271014f85732b802a02a9d3e18f66c3b42008
Patrick <hsiang851108@gmail.com>
2024-04-29 22:01:12 +0800

    Update install homebrew command

--- scripts/install_homebrew.sh
+++ scripts/install_homebrew.sh
@@ -10,8 +10,8 @@ fi
 which -s brew
 if [[ $? != 0 ]] ; then
     print 'Install Homebrew'
-    run_cmd '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
-    # run_cmd '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+    # run_cmd '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    run_cmd '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
 else
     print 'Update Homebrew'
     brew update
