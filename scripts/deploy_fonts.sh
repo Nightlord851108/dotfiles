fa57e3e0f886efb1b02fd0e976fd30576ce4f2a8
Patrick <nightlord851108@gmail.com>
2025-07-09 11:38:20 +0800

    The commands for deploy font is updated to the latest setting

--- scripts/deploy_fonts.sh
+++ scripts/deploy_fonts.sh
@@ -2,11 +2,11 @@
 source ./utils.sh
 
 print 'Homebrew Tap Fonts'
-run_cmd 'brew tap homebrew/cask-fonts'
+# run_cmd 'brew tap homebrew/cask-fonts'
 # cask_install 'font-hack-nerd-font'
-cask_install 'font-sourcecodepro-nerd-font'
-cask_install 'font-lxgw-wenkai-tc.rb'
-cask_install 'font-lxgw-wenkai-mono-tc.rb'
-cask_install 'font-lxgw-wenkai.rb'
-cask_install 'font-lxgw-wenkai-light.rb'
-cask_install 'font-lxgw-wenkai-gb.rb'
+cask_install 'font-sauce-code-pro-nerd-font'
+cask_install 'font-lxgw-wenkai-tc'
+cask_install 'font-lxgw-wenkai-mono-tc'
+cask_install 'font-lxgw-wenkai'
+cask_install 'font-lxgw-wenkai-light'
+cask_install 'font-lxgw-wenkai-gb-lite'
